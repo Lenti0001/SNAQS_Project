9028206570f963069c25c62522648f299538acfa
Florent Imishti Mustafaj <lentiflorent@hotmail.com>
2025-04-23 03:36:32 +0200

    v0.1 - Added XPCA classification software support
    The code now runs the classification software on the SNAQS object, if the run_xpca_classification() is called - and stores to a new attribute for the SNAQS_objects.

--- main_script.py
+++ main_script.py
@@ -45,27 +45,27 @@ class SNAQS_object():
         plt.plot(self.wave, self.flux+self.error, "--", color="red", alpha=0.5)
         plt.plot(self.wave, self.flux-self.error, "--", color="red", alpha=0.1, label="Observed spec. errors")
         if param_type=="SMC":
-            plt.plot(self.wave, smc(self.wave, self.compoM_SMC["z"], self.compoM_SMC["AB"], self.compoM_SMC["normalisation"]), color="blue", label="Fitted model - {} params".format(param_type))
+            plt.plot(self.wave, smc(self.wave, self.compoM_SMC["z"], self.compoM_SMC["AB"], self.compoM_SMC["norm"]), color="blue", label="Fitted model - {} params".format(param_type))
             
             plt.plot([], [], ' ', label="$\chi²_{red} = $" + "{}".format(int(self.compoM_SMC["red_chi2"])))
-            plt.plot([], [], ' ', label="Norm = {} $\pm$ {}".format(np.round(self.compoM_SMC["normalisation"], find_decimal_point(self.compoM_SMC["normalisation_std"])), np.round(self.compoM_SMC["normalisation_std"], find_decimal_point(self.compoM_SMC["normalisation_std"]))))
+            plt.plot([], [], ' ', label="Norm = {} $\pm$ {}".format(np.round(self.compoM_SMC["norm"], find_decimal_point(self.compoM_SMC["norm_std"])), np.round(self.compoM_SMC["norm_std"], find_decimal_point(self.compoM_SMC["norm_std"]))))
             plt.plot([], [], ' ', label="AB = {} $\pm$ {}".format(np.round(self.compoM_SMC["AB"], find_decimal_point(self.compoM_SMC["AB_std"])), np.round(self.compoM_SMC["AB_std"], find_decimal_point(self.compoM_SMC["AB_std"]))))
             plt.plot([], [], ' ', label="z = {} $\pm$ {}".format(np.round(self.compoM_SMC["z"], find_decimal_point(self.compoM_SMC["z_std"])), np.round(self.compoM_SMC["z_std"], find_decimal_point(self.compoM_SMC["z_std"]))))
             plt.plot([], [], ' ', label="$z_{header} = $" + "{}".format(self.z_header))
         
         elif param_type=="LMC":
-            plt.plot(self.wave, lmc(self.wave, self.compoM_LMC["z"], self.compoM_LMC["AB"], self.compoM_LMC["normalisation"]), color="blue", label="Fitted model - {} params".format(param_type))
+            plt.plot(self.wave, lmc(self.wave, self.compoM_LMC["z"], self.compoM_LMC["AB"], self.compoM_LMC["norm"]), color="blue", label="Fitted model - {} params".format(param_type))
             
             plt.plot([], [], ' ', label="$\chi²_{red} = $" + "{}".format(int(self.compoM_LMC["red_chi2"])))
-            plt.plot([], [], ' ', label="Norm = {} $\pm$ {}".format(np.round(self.compoM_LMC["normalisation"], find_decimal_point(self.compoM_LMC["normalisation_std"])), np.round(self.compoM_LMC["normalisation_std"], find_decimal_point(self.compoM_LMC["normalisation_std"]))))
+            plt.plot([], [], ' ', label="Norm = {} $\pm$ {}".format(np.round(self.compoM_LMC["norm"], find_decimal_point(self.compoM_LMC["norm_std"])), np.round(self.compoM_LMC["norm_std"], find_decimal_point(self.compoM_LMC["norm_std"]))))
             plt.plot([], [], ' ', label="AB = {} $\pm$ {}".format(np.round(self.compoM_LMC["AB"], find_decimal_point(self.compoM_LMC["AB_std"])), np.round(self.compoM_LMC["AB_std"], find_decimal_point(self.compoM_LMC["AB_std"]))))
             plt.plot([], [], ' ', label="z = {} $\pm$ {}".format(np.round(self.compoM_LMC["z"], find_decimal_point(self.compoM_LMC["z_std"])), np.round(self.compoM_LMC["z_std"], find_decimal_point(self.compoM_LMC["z_std"]))))
             plt.plot([], [], ' ', label="$z_{header} = $" + "{}".format(self.z_header))
         elif param_type=="MW":
-            plt.plot(self.wave, mw(self.wave, self.compoM_MW["z"], self.compoM_MW["AB"], self.compoM_MW["normalisation"]), color="blue", label="Fitted model - {} params".format(param_type))
+            plt.plot(self.wave, mw(self.wave, self.compoM_MW["z"], self.compoM_MW["AB"], self.compoM_MW["norm"]), color="blue", label="Fitted model - {} params".format(param_type))
             
             plt.plot([], [], ' ', label="$\chi²_{red} = $" + "{}".format(int(self.compoM_MW["red_chi2"])))
-            plt.plot([], [], ' ', label="Norm = {} $\pm$ {}".format(np.round(self.compoM_MW["normalisation"], find_decimal_point(self.compoM_MW["normalisation_std"])), np.round(self.compoM_MW["normalisation_std"], find_decimal_point(self.compoM_MW["normalisation_std"]))))
+            plt.plot([], [], ' ', label="Norm = {} $\pm$ {}".format(np.round(self.compoM_MW["norm"], find_decimal_point(self.compoM_MW["norm_std"])), np.round(self.compoM_MW["norm_std"], find_decimal_point(self.compoM_MW["norm_std"]))))
             plt.plot([], [], ' ', label="AB = {} $\pm$ {}".format(np.round(self.compoM_MW["AB"], find_decimal_point(self.compoM_MW["AB_std"])), np.round(self.compoM_MW["AB_std"], find_decimal_point(self.compoM_MW["AB_std"]))))
             plt.plot([], [], ' ', label="z = {} $\pm$ {}".format(np.round(self.compoM_MW["z"], find_decimal_point(self.compoM_MW["z_std"])), np.round(self.compoM_MW["z_std"], find_decimal_point(self.compoM_MW["z_std"]))))
             plt.plot([], [], ' ', label="$z_{header} = $" + "{}".format(self.z_header))
@@ -89,7 +89,6 @@ class SNAQS():
     def __init__(self, path, RA_range=[190, 210], DEC_range=[22, 36]):
         self.path = path
         self.dir_list = os.listdir(path)
-        
         self.SNAQS_list = []
         
     
@@ -127,24 +126,11 @@ class SNAQS():
                 m.hesse()
             
             ### Create new attributes related to the fitting procedure:
-            self.objects[i].compoM_SMC = {}
-            
-            self.objects[i].compoM_SMC["z"] = m.values["z"]
-            self.objects[i].compoM_SMC["z_std"] = m.errors["z"]
-            self.objects[i].compoM_SMC["AB"] = m.values["AB"]
-            self.objects[i].compoM_SMC["AB_std"] = m.errors["AB"]
-            self.objects[i].compoM_SMC["chi2"] = m.fval
-            self.objects[i].compoM_SMC["red_chi2"] = m.fval/m.ndof
-            self.objects[i].compoM_SMC["ndof"] = m.ndof
-            self.objects[i].compoM_SMC["p_val"] = chi2.sf(m.fval, m.ndof)
-            
-            self.objects[i].compoM_SMC["normalisation"] = m.values["normalisation"]
-            self.objects[i].compoM_SMC["normalisation_std"] = m.errors["normalisation"]
+            self.objects[i].compoM_SMC = {"z": m.values["z"], "z_std": m.errors["z"], "AB": m.values["AB"], "AB_std": m.errors["AB"], "chi2": m.fval, "red_chi2": m.fval/m.ndof, "ndof": m.ndof, "p_val": chi2.sf(m.fval, m.ndof), "norm": m.values["normalisation"], "norm_std": m.errors["normalisation"]}
             
             if save_plots==True:
                 self.objects[i].plot_compoM(param_type="SMC")
             
-        return
     
     def fit_compoM_LMC(self, save_plots=True):
         '''Fits the loaded spectra with the SMC parameters. New atributes are then added to the SNAQS objects related to the fitting when done.'''
@@ -165,24 +151,11 @@ class SNAQS():
                 m.hesse()
             
             ### Create new attributes related to the fitting procedure:
-            self.objects[i].compoM_LMC = {}
-            
-            self.objects[i].compoM_LMC["z"] = m.values["z"]
-            self.objects[i].compoM_LMC["z_std"] = m.errors["z"]
-            self.objects[i].compoM_LMC["AB"] = m.values["AB"]
-            self.objects[i].compoM_LMC["AB_std"] = m.errors["AB"]
-            self.objects[i].compoM_LMC["chi2"] = m.fval
-            self.objects[i].compoM_LMC["red_chi2"] = m.fval/m.ndof
-            self.objects[i].compoM_LMC["ndof"] = m.ndof
-            self.objects[i].compoM_LMC["p_val"] = chi2.sf(m.fval, m.ndof)
-            
-            self.objects[i].compoM_LMC["normalisation"] = m.values["normalisation"]
-            self.objects[i].compoM_LMC["normalisation_std"] = m.errors["normalisation"]
+            self.objects[i].compoM_LMC = {"z": m.values["z"], "z_std": m.errors["z"], "AB": m.values["AB"], "AB_std": m.errors["AB"], "chi2": m.fval, "red_chi2": m.fval/m.ndof, "ndof": m.ndof, "p_val": chi2.sf(m.fval, m.ndof), "norm": m.values["normalisation"], "norm_std": m.errors["normalisation"]}
             
             if save_plots==True:
                 self.objects[i].plot_compoM(param_type="LMC")
             
-        return
     
     def fit_compoM_MW(self, save_plots=True):
         '''Fits the loaded spectra with the SMC parameters. New atributes are then added to the SNAQS objects related to the fitting when done.'''
@@ -203,24 +176,24 @@ class SNAQS():
                 m.hesse()
             
             ### Create new attributes related to the fitting procedure:
-            self.objects[i].compoM_MW = {}
-            
-            self.objects[i].compoM_MW["z"] = m.values["z"]
-            self.objects[i].compoM_MW["z_std"] = m.errors["z"]
-            self.objects[i].compoM_MW["AB"] = m.values["AB"]
-            self.objects[i].compoM_MW["AB_std"] = m.errors["AB"]
-            self.objects[i].compoM_MW["chi2"] = m.fval
-            self.objects[i].compoM_MW["red_chi2"] = m.fval/m.ndof
-            self.objects[i].compoM_MW["ndof"] = m.ndof
-            self.objects[i].compoM_MW["p_val"] = chi2.sf(m.fval, m.ndof)
-            
-            self.objects[i].compoM_MW["normalisation"] = m.values["normalisation"]
-            self.objects[i].compoM_MW["normalisation_std"] = m.errors["normalisation"]
+            self.objects[i].compoM_MW = {"z": m.values["z"], "z_std": m.errors["z"], "AB": m.values["AB"], "AB_std": m.errors["AB"], "chi2": m.fval, "red_chi2": m.fval/m.ndof, "ndof": m.ndof, "p_val": chi2.sf(m.fval, m.ndof), "norm": m.values["normalisation"], "norm_std": m.errors["normalisation"]}
             
             if save_plots==True:
                 self.objects[i].plot_compoM(param_type="MW")
                 
-        return
+    def run_xpca_classification(self):
+        for i in tqdm(self.SNAQS_list):
+            try:
+                os.system("python -m xpca {} -s sdss -o {}temp".format(self.path + i, self.path))
+            except:
+                raise Exception("FAILED - Either the XPCA software is not installed, or there is a bug in the software preventing a classification.")
+            hdu = fits.open("{}temp".format(self.path))
+            self.objects[i].xpca = {"zBest": hdu[1].data["zBest"], "zBestErr": hdu[1].data["zBestErr"], "zBestChi2": hdu[1].data["zBestChi2"], "zBestType": hdu[1].data["zBestType"], "zBestSubType": hdu[1].data["zBestSubType"]}
+            os.system("rm {}temp".format(self.path))
+                    
+        
+                
+    
         
             
             
